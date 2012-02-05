0827d4049bc6fd3722bbb4fef1ffa78f65f70fbc
augustuswm <gusmayo@gmail.com>
2012-02-04 19:54:06 -0600

    Remove old package

--- src/com/unnamed/dreamscape/RunApp.java
+++
@@ -1,9 +0,0 @@
-package com.unnamed.dreamscape;
-
-import com.badlogic.gdx.backends.jogl.JoglApplication;
-
-public class RunApp {
-	  public static void main (String[] argv) {
-          new JoglApplication(new App(), "Dreamscape", 480, 320, false);               
-  }
-}
